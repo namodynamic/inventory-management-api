590a16f052c80eb3e985b8c07911ba4d301c687c
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-03-22 20:29:25 +0100

    Add python-decouple for env variable management

--- inventory_management_api/settings.py
+++ inventory_management_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$3qb@3^jo-5kw$hash675lz36(_79ygdih@61^2ehu@lauj_mj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "inventory-api",
         "USER": "postgres",
-        "PASSWORD": "Cyprian65",
+        "PASSWORD": config('DB_PASSWORD'),
         "HOST": "localhost",
         "PORT": "5433",
     }
