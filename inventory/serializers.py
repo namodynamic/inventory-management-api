710d8e5aded9e8bc9772d1d2dcab03f58a2d696b
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-04-07 22:17:50 +0100

    Update UserSerializer to remove is_staff and is_superuser fields during user creation and update

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -7,23 +7,25 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'is_staff')
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
         extra_kwargs = {
             'password': {'write_only': True}
         }
     
     def create(self, validated_data):
+        validated_data.pop('is_staff', None)
         user = User.objects.create_user(**validated_data)
         return user
     
     def update(self, instance, validated_data):
+        validated_data.pop('is_staff', None)
+        validated_data.pop('is_superuser', None)
         password = validated_data.pop('password', None)
         user = super().update(instance, validated_data)
         if password:
             user.set_password(password)
             user.save()
-        return user  
-    
+        return user   
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
